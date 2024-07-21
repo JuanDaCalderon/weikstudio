a1966f353954a48a477f2be4f2bc6a227b36f419
juan david calderon jimenez <j.calderonj@uniandes.edu.co>
2024-07-21 15:01:44 -0500

    first commit

---
+++ functions.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * weikstudio functions and definitions
+ *
+ *
+ * @package weikstudio
+ */
+
+/**
+ * Enqueue scripts and styles.
+ */
+if ( ! function_exists( 'weikstudio_bootstrap_scripts' ) ) :
+	/**
+	 * Enqueue Bootstrap scripts and styles.
+	 *
+	 * @return void
+	 */
+	function weikstudio_bootstrap_scripts() {
+		wp_enqueue_style('weikstudio-bootstrap', get_parent_theme_file_uri('assets/css/bootstrap.min.css'), array(), wp_get_theme()->get('Version'), 'all');
+		wp_enqueue_style('weikstudio-global', get_parent_theme_file_uri('assets/css/weikstudio/global.css'));
+		wp_dequeue_script( 'all' );
+		wp_enqueue_script('weikstudio-bootstrap', get_parent_theme_file_uri('assets/js/bootstrap.bundle.min.js'), array(), wp_get_theme()->get('Version'), true);
+	}
+endif;
+
+add_action( 'wp_enqueue_scripts', 'weikstudio_bootstrap_scripts' );
---
+++ patterns/footer.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Title: Footer
+ * Slug: weikstudio/footer
+ * Categories: footer
+ * Block Types: core/template-part/footer
+ * Description: The footer section.
+ */
+?>
+
+<!-- wp:group -->
+<div class="footer-ws container d-flex flex-column text-center text-lg-start px-lg-0 mt-3 pt-4 pb-5">
+	<!-- wp:group -->
+	<div class="ms-2 d-flex flex-column align-items-center align-items-lg-start">
+		<!-- wp:paragraph -->
+		<p class="py-0 my-0">WEIK MOTION STUDIO SAS</p>
+		<!-- /wp:paragraph -->
+		<!-- wp:paragraph -->
+		<a class="py-0 my-0" href="mailto:weik@weikstudio.com"> MAIL: weik@weikstudio.com </a>
+		<!-- /wp:paragraph -->
+		<!-- wp:paragraph -->
+		<a class="py-0 my-0" href="tel:+57304-364-9269"> PHONE: +57 304 364 9269 </a>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:group -->
+	<!-- wp:group -->
+	<div class="footer-ws-divider">
+		<!-- wp:separator -->
+		<hr/>
+		<!-- /wp:separator -->
+	</div>
+	<!-- /wp:group -->
+	<!-- wp:group -->
+	<div class="d-flex flex-lg-row flex-column text-center justify-content-center justify-content-lg-between ms-2 mt-2">
+		<!-- wp:group -->
+		<div>
+			<!-- wp:paragraph -->
+			<p class="footer-copyright col-12 py-0 my-0 mt-1"> Copyrigth &#169; 2019 Weik Motion Studio SAS </p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:group -->
+		<!-- wp:group -->
+		<div class="row mt-3 mt-lg-0">
+			<!-- wp:column -->
+			<div class="col">
+				<!-- wp:paragraph -->
+				<a class="d-flex social-icon" href="https://www.facebook.com/WeikStudio/?locale=es_LA" target="_blank">
+					<!-- wp:image -->
+					<img src="<?php echo esc_url(get_theme_file_uri('assets/images/redes-sociales/facebook.png')); ?>" class="img img-fluid" loading="lazy" alt="weik studio facebook"/>
+					<!-- /wp:image -->
+				</a>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:column -->
+			<!-- wp:column -->
+			<div class="col">
+				<!-- wp:paragraph -->
+				<a class="d-flex social-icon" href="https://twitter.com/weikstudio" target="_blank">
+					<!-- wp:image -->
+					<img src="<?php echo esc_url(get_theme_file_uri('assets/images/redes-sociales/Twitter.png')); ?>" class="img img-fluid" loading="lazy" alt="weik studio twitter"/>
+					<!-- /wp:image -->
+				</a>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:column -->
+			<!-- wp:column -->
+			<div class="col">
+				<!-- wp:paragraph -->
+				<a class="d-flex social-icon" href="https://www.instagram.com/weikstudio/?hl=es" target="_blank">
+					<!-- wp:image -->
+					<img src="<?php echo esc_url(get_theme_file_uri('assets/images/redes-sociales/instagram.png')); ?>" class="img img-fluid" loading="lazy" alt="weik studio instagram"/>
+					<!-- /wp:image -->
+				</a>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:column -->
+			<!-- wp:column -->
+			<div class="col">
+				<!-- wp:paragraph -->
+				<a class="d-flex social-icon" href="https://vimeo.com/weikstudio" target="_blank">
+					<!-- wp:image -->
+					<img src="<?php echo esc_url(get_theme_file_uri('assets/images/redes-sociales/vimeo.png')); ?>" class="img img-fluid" loading="lazy" alt="weik studio vimeo"/>
+					<!-- /wp:image -->
+				</a>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:column -->
+			<!-- wp:column -->
+			<div class="col">
+				<!-- wp:paragraph -->
+				<a class="d-flex social-icon" href="https://www.linkedin.com/company/weik-studio/" target="_blank">
+					<!-- wp:image -->
+					<img src="<?php echo esc_url(get_theme_file_uri('assets/images/redes-sociales/linkedin.png')); ?>" class="img img-fluid" loading="lazy" alt="weik studio linkedin"/>
+					<!-- /wp:image -->
+				</a>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:column -->
+			<!-- wp:column -->
+			<div class="col">
+				<!-- wp:paragraph -->
+				<a class="d-flex social-icon" href="https://www.youtube.com/@weikmotionstudio6782" target="_blank">
+					<!-- wp:image -->
+					<img src="<?php echo esc_url(get_theme_file_uri('assets/images/redes-sociales/you-tube.png')); ?>" class="img img-fluid" loading="lazy" alt="weik studio youtube"/>
+					<!-- /wp:image -->
+				</a>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:column -->
+		</div>
+		<!-- /wp:group -->
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
