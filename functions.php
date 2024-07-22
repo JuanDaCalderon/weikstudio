06ef2410daa57ec6955257deefbe1d35c311b50c
juan david calderon jimenez <j.calderonj@uniandes.edu.co>
2024-07-21 23:19:15 -0500

    updated with header

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
  * @package weikstudio
  */
 
-/**
+ /**
  * Enqueue scripts and styles.
  */
 if ( ! function_exists( 'weikstudio_bootstrap_scripts' ) ) :
@@ -18,6 +18,8 @@ if ( ! function_exists( 'weikstudio_bootstrap_scripts' ) ) :
 	function weikstudio_bootstrap_scripts() {
 		wp_enqueue_style('weikstudio-bootstrap', get_parent_theme_file_uri('assets/css/bootstrap.min.css'), array(), wp_get_theme()->get('Version'), 'all');
 		wp_enqueue_style('weikstudio-global', get_parent_theme_file_uri('assets/css/weikstudio/global.css'));
+		wp_enqueue_style('weikstudio-header', get_parent_theme_file_uri('assets/css/weikstudio/header.css'));
+		wp_enqueue_style('weikstudio-footer', get_parent_theme_file_uri('assets/css/weikstudio/footer.css'));
 		wp_dequeue_script( 'all' );
 		wp_enqueue_script('weikstudio-bootstrap', get_parent_theme_file_uri('assets/js/bootstrap.bundle.min.js'), array(), wp_get_theme()->get('Version'), true);
 	}
